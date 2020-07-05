c7383009a2caf79b09a2a8908b16bf70c4010ebb
Saatvik Shukla <saatvik.shukla@inmobi.com>
2020-07-05 08:54:30 +0530

    Update weekly forecast with icons

--- src/components/WeeklyForecast/index.jsx
+++ src/components/WeeklyForecast/index.jsx
@@ -6,11 +6,17 @@ import "./index.css";
 export default function WeeklyForecast({
   dailyData
 }) {
-  console.log(dailyData);
   const epochToDate = (targetDate) => {
-    return moment.unix(targetDate).format('dddd')
+    return moment.unix(targetDate).format('ddd')
   };
-
+  function roundToTwoDecimal(number) {
+    try {
+      return Number(Number(number).toFixed(0));
+    } catch (err) {
+      console.log('err', err);
+      return 0;
+    }
+  }
 
   return (
     <>
@@ -19,9 +25,13 @@ export default function WeeklyForecast({
           return (
             <>
               <div className="dailyForeCastCard">
-                <p>{item.temp.min}</p>
-                <p>{item.temp.max}</p>
-                <p>{epochToDate(item.dt)}</p>
+                <div>{epochToDate(item.dt)}</div>
+                <div className="minMaxWrapper">
+                  <div className="minMaxTemp">{roundToTwoDecimal(item.temp.min) + "°"}</div>
+                  <div className="minMaxTemp">{roundToTwoDecimal(item.temp.max) + "°"}</div>
+                </div>
+                <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`} />
+                <div>{item.weather[0].main}</div>
               </div>
             </>
           );
