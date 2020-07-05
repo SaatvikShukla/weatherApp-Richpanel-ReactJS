3d611a364796a188b9f531af5c8f4e32c0b362f4
Saatvik Shukla <saatvik.shukla@inmobi.com>
2020-07-05 19:26:26 +0530

    update data handler

--- src/components/DataHandler/index.jsx
+++ src/components/DataHandler/index.jsx
@@ -2,23 +2,44 @@ import React, { useEffect, useState } from 'react';
 import { getWeatherDetailsFromLocation } from "./DataHandler.service";
 import ShowIfPropTrue from "../../utils/ShowIfPropTrue";
 import WeeklyForecast from "../WeeklyForecast";
+import CurrentWeatherCard from "../CurrentWeatherCard";
+import HeaderSearch from "../HeaderSearch";
 
 export default function DataHandler({
   latLong
 }) {
 
   const [weatherData, setWeatherData] = useState([]);
+  const [hourlyDataArray, sethourlyDataArray] = useState([]);
   const [showLoader, setShowLoader] = useState(false);
+
+  useEffect(() => {
+    if (weatherData['hourly']) {
+      console.log('running useeffect');
+      let _hourlyDataArray = [...hourlyDataArray];
+      let _hourlyTemp = [];
+      let _hourlyTime = [];
+      weatherData['hourly'].forEach((item) => {
+        _hourlyTemp.push(item.temp);
+        _hourlyTime.push(item.dt);
+      })
+      sethourlyDataArray({
+        temp: _hourlyTemp,
+        time: _hourlyTime
+      });
+    }
+  }, [weatherData]);
 
   useEffect(() => {
     setShowLoader(true);
     getWeatherDetailsFromLocation({ lattitude: latLong.lattitude, longitude: latLong.longitude }).then((resp) => {
       setWeatherData(resp);
+      console.log('12345678', resp);
       setShowLoader(false);
     })
   }, []);
 
-
+  // console.log(weatherData);
   return (
     <>
       <ShowIfPropTrue prop={showLoader}>
@@ -26,12 +47,13 @@ export default function DataHandler({
       </ShowIfPropTrue>
       <ShowIfPropTrue prop={!showLoader}>
         <>
-          Location is received, fetching data
           <div>
-            {/* {JSON.stringify(weatherData)} */}
-            {/* <HeaderSearch /> */}
+            <HeaderSearch />
             <WeeklyForecast dailyData={weatherData.daily} />
-            {/* <CurrentWeatherCard /> */}
+            <CurrentWeatherCard
+              currentData={weatherData.current}
+              hourlyDataArray={hourlyDataArray}
+            />
           </div>
         </>
       </ShowIfPropTrue>
